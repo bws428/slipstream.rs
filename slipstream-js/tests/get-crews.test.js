e66d5739f56a5150f7fd648418fb873105abf448
Brian D. Wendt <briandwendt@gmail.com>
2024-08-26 20:10:41 -0400

    Delete slipstream-js directory

--- slipstream-js/tests/get-crews.test.js
+++
@@ -1,6 +0,0 @@
-import getCrews from "../src/get-crews";
-import { hdnCrewData, crewNames } from "./test-data";
-
-test('Returns a properly-formatted crewNames object.', () => {
-  expect(getCrews(hdnCrewData)).toStrictEqual(crewNames);
-});
--- slipstream-js/tests/get-flights.test.js
+++
@@ -1,6 +0,0 @@
-import getFlights from "../src/get-flights";
-import { gridText, gridFlights  } from "./test-data";
-
-test('Returns a properly-formatted flights object with EMPTY crews[].', () => {
-  expect(getFlights(gridText)).toStrictEqual(gridFlights);
-});
